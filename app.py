f8a5292a30a387a92ca71f7dbd46cb146e5594d3
ankey207 <amanyankey040@gmail.com>
2024-03-05 10:40:30 +0000

    return to chrome

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ from selenium.webdriver.chrome.options import Options
 import streamlit_antd_components as sac
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+
+
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-notifications")
@@ -23,12 +26,16 @@ import time
 import function
 import os, sys
 
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-opts.add_argument("--disable-notifications")
-opts.add_argument('--disable-gpu')
-opts.add_argument("--disable-popup-blocking")
+#from selenium.webdriver import FirefoxOptions
+#opts = FirefoxOptions()
+#opts.add_argument("--headless")
+#opts.add_argument("--disable-notifications")
+#opts.add_argument("--disable-popup-blocking")
+
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+
 
 st.set_page_config(page_title="SmartSMS",layout="wide", initial_sidebar_state="auto", page_icon="logo_SmartSMS.png")
 hide_st_style = """
@@ -39,7 +46,11 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-service = Service(GeckoDriverManager().install())
+
+#service = Service(GeckoDriverManager().install())
+
+
+
 
 st.header("SMARTSMS: PERSONNALISEZ ET ENVOYEZ DES SMS EN MASSE FACILEMENT")
 function.load_styles()
@@ -149,8 +160,9 @@ if segment=="SmartSMS":
                         messages.append(message)
 
                 #lancement du navigateur
+                driver =get_driver()
                 #driver =uc.Chrome(options=chrome_options)
-                driver =webdriver.Firefox(options=opts,service=service)#driver_executable_path="./chromedriver.exe",
+                #driver =webdriver.Firefox(options=opts,service=service)#driver_executable_path="./chromedriver.exe",
                 driver.set_window_size(650,750)
                 driver.get("https://messages.google.com/web/")
 
